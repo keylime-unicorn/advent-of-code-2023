8a85ac3f0ea94e94d0a21a7d9c2984b6a72a29bd
Theo Thompson <25823466+theot12@users.noreply.github.com>
2023-12-08 01:15:22 -0800

    day04 complete

--- day04/part1.py
+++ day04/part1.py
@@ -1,4 +1,25 @@
 #!/opt/homebrew/bin/python3
 # Advent of Code 2023
 # Alex Thompson
-# Day XX Part 1
+# Day 04 Part 1
+
+import sys
+
+total_points = 0
+
+for line in sys.stdin:
+    line = line.strip().split(": ")
+    card = line[1].split(" | ")
+
+    winning = card[0].split()
+    selected = card[1].split()
+
+    matches = 0
+    for number in winning:
+        if number in selected:
+            matches += 1
+
+    if matches > 0:
+        total_points += 2**(matches-1)
+    
+print("points: " + str(total_points))
--- day04/part2.py
+++ day04/part2.py
@@ -1,4 +1,36 @@
 #!/opt/homebrew/bin/python3
 # Advent of Code 2023
 # Alex Thompson
-# Day XX Part 2
+# Day 04 Part 2
+
+import sys
+
+card_id = 0
+card_count = {}
+
+for line in sys.stdin:
+
+    card_id += 1
+    if card_id not in card_count:
+        card_count[card_id] = 0
+    card_count[card_id] += 1
+
+    line = line.strip().split(": ")
+    card = line[1].split(" | ")
+
+    winning = card[0].split()
+    selected = card[1].split()
+
+    matches = 0
+    for number in winning:
+        if number in selected:
+            matches += 1
+
+    for copy in range(card_count[card_id]):
+        for i in range(1, matches+1):
+            copy_id = card_id + i
+            if copy_id not in card_count:
+                card_count[copy_id] = 0
+            card_count[copy_id] += 1
+
+print("total cards: " + str(sum(card_count.values())))
