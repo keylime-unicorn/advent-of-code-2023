f4be1b5a31ec3056587e0e6a378adde86e72f3f2
Theo Thompson <25823466+theot12@users.noreply.github.com>
2023-12-07 22:46:24 -0800

    day02 complete

--- day02/part1.py
+++ day02/part1.py
@@ -1,4 +1,46 @@
 #!/opt/homebrew/bin/python3
 # Advent of Code 2023
 # Alex Thompson
-# Day XX Part 1
+# Day 02 Part 1
+
+import sys
+
+max_red = 12
+max_green = 13
+max_blue = 14
+
+game_sum = 0
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(": ")
+    gameid = int(line[0][5:])
+
+    game = line[1].split("; ")
+    game_impossible = False
+
+    for hand in game: 
+
+        blocks = hand.split(", ")
+
+        for b in blocks:
+            b = b.split(" ")
+            count = int(b[0])
+            color = b[1]
+
+            if color == "red" and count > max_red:
+                game_impossible = True
+                break
+            elif color == "blue" and count > max_blue:
+                game_impossible = True
+                break
+            elif color == "green" and count > max_green:
+                game_impossible = True
+                break
+
+        if game_impossible:
+            break
+
+    if game_impossible == False:
+        game_sum += gameid
+
+print("sum: " + str(game_sum))
--- day02/part2.py
+++ day02/part2.py
@@ -1,4 +1,36 @@
 #!/opt/homebrew/bin/python3
 # Advent of Code 2023
 # Alex Thompson
-# Day XX Part 2
+# Day 02 Part 2
+
+import sys
+
+powers_sum = 0
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(": ")
+    game = line[1].split("; ")
+
+    max_red = 0
+    max_blue = 0
+    max_green = 0
+    for hand in game: 
+
+        blocks = hand.split(", ")
+
+        for b in blocks:
+            b = b.split(" ")
+            count = int(b[0])
+            color = b[1]
+
+            if color == "red" and count > max_red:
+                max_red = count
+            elif color == "blue" and count > max_blue:
+                max_blue = count
+            elif color == "green" and count > max_green:
+                max_green = count
+
+    game_power = max_red * max_green * max_blue
+    powers_sum += game_power
+
+print("total power: " + str(powers_sum))
